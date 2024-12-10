eaa4e73c1b0d6ecd98ae1d72f56591b12912d79d
parangat11 <parangataditya001@gmail.com>
2024-12-10 17:08:46 +0530

    Ability to add multiple dropdowns added.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
 import "./style.css"
 
-const dropdown = document.querySelector(".dropdown");
-const dropdownButton = document.querySelector(".dropdown-button");
-const options = document.querySelectorAll('.option');
+const dropdowns = document.querySelectorAll(".dropdown");
 
-console.log(options);
-
-let currentSelection = null;
-
-dropdownButton.addEventListener('click', (e) => {
-    if(dropdown.classList.contains('open')) {
-        dropdown.classList.remove('open');
-        let str = dropdownButton.textContent;
-        dropdownButton.textContent = "";
-        for(let i = 0; i < str.length - 1; i++) {
-            dropdownButton.textContent += str[i];
+dropdowns.forEach(dropdown => {
+    console.log(dropdown);
+    const dropdownButton = dropdown.querySelector('.dropdown-button');
+    console.log(dropdownButton)
+    const options = dropdown.querySelectorAll('.option');
+    dropdownButton.textContent += "▼";
+    dropdownButton.addEventListener('click', (e) => {
+        if(dropdown.classList.contains('open')) {
+            dropdown.classList.remove('open');
+            let str = dropdownButton.textContent;
+            dropdownButton.textContent = "";
+            for(let i = 0; i < str.length - 1; i++) {
+                dropdownButton.textContent += str[i];
+            }
+            dropdownButton.textContent += "▼";
         }
-        dropdownButton.textContent += "▼";
-    }
-    else {
-        dropdown.classList.add('open');
-        let str = dropdownButton.textContent;
-        dropdownButton.textContent = "";
-        for(let i = 0; i < str.length - 1; i++) {
-            dropdownButton.textContent += str[i];
+        else {
+            dropdown.classList.add('open');
+            let str = dropdownButton.textContent;
+            dropdownButton.textContent = "";
+            for(let i = 0; i < str.length - 1; i++) {
+                dropdownButton.textContent += str[i];
+            }
+            dropdownButton.textContent += "▲";
         }
-        dropdownButton.textContent += "▲";
-    }
-})
-
-options.forEach((option) => {
-    option.addEventListener('click', (e) => {
-        dropdownButton.textContent = option.textContent;
-        dropdownButton.textContent += "▲";
-        dropdownButton.click();
+    })
+    options.forEach((option) => {
+        option.addEventListener('click', (e) => {
+            dropdownButton.textContent = option.textContent;
+            dropdownButton.textContent += "▲";
+            dropdownButton.click();
+        })
     })
 })
